c37d68a42b57aa54a79a09ca0ae38b5f505319e2
ledoc <ledoc@lucas-sanchez.fr>
2025-10-16 16:51:53 +0200

    feat proto test register

--- build.rs
+++ build.rs
@@ -2,6 +2,13 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     tonic_prost_build::configure()
         .build_server(true)   // Génère le serveur
         .build_client(true)   // Génère le client
-        .compile_protos(&["proto/register.proto"], &["proto"])?;
+        .compile_protos(
+            &[
+                "proto/registertest.proto",
+                "proto/register.proto",
+                "proto/login.proto"
+            ], 
+            &["proto"]
+        )?;
     Ok(())
 }
